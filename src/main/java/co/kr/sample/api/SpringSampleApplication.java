3c51d388a29533935dc781a6a936fc8e771d8538
hku2002 <hku2002@hanmail.net>
2024-01-10 21:09:03 +0900

    feat: first commit

---
+++ src/main/java/co/kr/sample/api/SpringSampleApplication.java
@@ -0,0 +1,13 @@
+package co.kr.sample.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/co/kr/sample/api/SpringSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package co.kr.sample.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
